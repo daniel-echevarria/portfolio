02a523ba3cc02cb9a0763bd4895787431c7f6b22
Kasper Bak <dan.echevarri@gmail.com>
2024-12-05 20:20:33 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <a className="text-grey-900 ">Daniel Echevarria</a>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/me.jpg"
+          alt="daniel echevarria"
+          width={1000}
+          height={400}
+          priority
+        />
         <Image
           className="dark:invert"
           src="/next.svg"
