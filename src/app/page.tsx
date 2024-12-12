c2f81c8e317f77421ff765f7ad8ef54b55d09d9f
Kasper Bak <dan.echevarri@gmail.com>
2024-12-12 16:27:58 +0100

    Add project cards to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,20 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import projects from "../data/projects.json";
+import Card from "@/components/Card";
 
 export default function Home() {
-  console.log(projects);
-  const CardList = () => {};
+  const CardList = () => {
+    const list = projects.map((project) => (
+      <Card
+        image={project.image}
+        title={project.title}
+        subtitle={project.subtitle}
+      />
+    ));
+    return list;
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <nav className="w-full justify-between flex text-grey-900 text-xl font-bold">
@@ -17,6 +27,7 @@ export default function Home() {
         </Link>
       </nav>
       <main className="w-full">
+        <CardList />
         <h1 className=" font-semibold text-7xl">Full Stack Web Developer</h1>
         <Image
           className="dark:invert"
