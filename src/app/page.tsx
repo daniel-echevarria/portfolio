4603af6c31ef8c9a616c21594424a3e256842ffc
Kasper Bak <dan.echevarri@gmail.com>
2024-12-13 17:10:31 +0100

    Add live links to projects to make it easy to try them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Card from "@/components/Card";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_300px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <nav className="w-full justify-between flex text-grey-900 text-xl font-bold">
+    <>
+      <nav className="z-10 w-full justify-between flex text-grey-900 text-xl font-bold p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <Link className="hover:underline" href="/">
           Daniel Echevarria
         </Link>
@@ -15,74 +15,79 @@ export default function Home() {
           About
         </Link>
       </nav>
-      <h1 className=" w-full font-semibold text-7xl">
-        Full Stack Web Developer. Programming Artist.
-      </h1>
+      <div className="grid grid-rows-[300px_1fr_20px] lg:grid-rows-[400px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="w-full font-semibold text-5xl md:text-6xl lg:text-7xl">
+          Full Stack Web Developer. <br />
+          Programming Artist.
+        </h1>
 
-      <main className="w-full">
-        <div className="grid grid-cols-2 gap-32">
-          <div className="flex flex-col gap-32">
-            {projects.map(
-              (project, index) =>
-                index % 2 === 0 && (
-                  <Card
-                    key={index}
-                    image={project.image}
-                    title={project.title}
-                    subtitle={project.subtitle}
-                  />
-                )
-            )}
+        <main className="w-full">
+          <div className="lg:grid grid-cols-2 gap-32">
+            <div className="flex flex-col gap-16 lg:gap-32">
+              {projects.map(
+                (project, index) =>
+                  index % 2 === 0 && (
+                    <Card
+                      key={index}
+                      image={project.image}
+                      title={project.title}
+                      live={project["live-link"]}
+                      subtitle={project.subtitle}
+                    />
+                  )
+              )}
+            </div>
+
+            <div className="flex flex-col lg:mt-72 gap-16 lg:gap-32 ">
+              {projects.map(
+                (project, index) =>
+                  index % 2 === 1 && (
+                    <Card
+                      key={index}
+                      image={project.image}
+                      live={project["live-link"]}
+                      title={project.title}
+                      subtitle={project.subtitle}
+                    />
+                  )
+              )}
+            </div>
           </div>
 
-          <div className="flex flex-col gap-32 mt-72">
-            {projects.map(
-              (project, index) =>
-                index % 2 === 1 && (
-                  <Card
-                    key={index}
-                    image={project.image}
-                    title={project.title}
-                    subtitle={project.subtitle}
-                  />
-                )
-            )}
+          <Image
+            className="dark:invert"
+            src="/me.jpg"
+            alt="daniel echevarria"
+            width={2428}
+            height={3238}
+            priority
+          />
+        </main>
+
+        <footer>
+          <div className="text-align: center; padding: 1rem; background-color: #f4f4f4;">
+            <p>&copy; 2024 Daniel Echevarria. All Rights Reserved.</p>
+            <p>
+              <a
+                href="https://github.com/daniel-echevarria"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>{" "}
+              '|
+              <a
+                href="https://www.linkedin.com/in/daniel-e-772683335"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>{" "}
+              |<a href="mailto:dan.echevarri@gmail.com">Contact</a>
+            </p>
           </div>
-        </div>
-
-        <Image
-          className="dark:invert"
-          src="/me.jpg"
-          alt="daniel echevarria"
-          width={2428}
-          height={3238}
-          priority
-        />
-      </main>
-
-      <footer>
-        <div className="text-align: center; padding: 1rem; background-color: #f4f4f4;">
-          <p>&copy; 2024 Daniel Echevarria. All Rights Reserved.</p>
-          <p>
-            <a
-              href="https://github.com/daniel-echevarria"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>{" "}
-            '|
-            <a
-              href="https://www.linkedin.com/in/daniel-e-772683335"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>{" "}
-            |<a href="mailto:dan.echevarri@gmail.com">Contact</a>
-          </p>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type ImageObj = {
   src: string;
@@ -9,22 +10,25 @@ type ImageObj = {
 
 type CardProps = {
   image: ImageObj;
+  live: string;
   title: string;
   subtitle: string;
 };
 
-const Card: React.FC<CardProps> = ({ image, title, subtitle }) => {
+const Card: React.FC<CardProps> = ({ image, title, live, subtitle }) => {
   return (
     <div className="flex flex-col items-center justify-center rounded-3xl relative overflow-hidden">
-      <div className="border transition-transform hover:scale-90 overflow-hidden duration-500 rounded-3xl">
-        <Image
-          className="p-10 bg-black transition-transform hover:scale-125 hover:cursor-pointer  duration-500 object-center object-cover border rounded-3xl"
-          src={image.src}
-          alt={image.alt}
-          width={image.width}
-          height={image.height}
-        />
-      </div>
+      <Link href={`${live}`}>
+        <button className="border transition-transform hover:scale-90 overflow-hidden duration-500 rounded-3xl">
+          <Image
+            className="p-10 bg-black transition-transform hover:scale-125 hover:cursor-pointer  duration-500 object-center object-cover border rounded-3xl"
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+          />
+        </button>
+      </Link>
       <p className="font-semibold text-xl mt-10">{title}</p>
       <p>{subtitle}</p>
     </div>
