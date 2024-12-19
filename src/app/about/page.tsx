c3c6ca6916f58e7f43b2903f37f4fafe71d4d19e
Kasper Bak <dan.echevarri@gmail.com>
2024-12-19 17:34:17 +0100

    Move nav and footer to layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Image from "next/image";
 
 export default function Page() {
-  return <p>About</p>;
+  return (
+    <main className="flex">
+      <header></header>
+      <Image
+        className=""
+        src={"/fun-me.jpg"}
+        alt="daniel echevarria thinking"
+        width={3060}
+        height={4080}
+      />
+      <p>About</p>
+    </main>
+  );
 }
