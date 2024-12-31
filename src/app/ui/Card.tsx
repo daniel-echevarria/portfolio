75d15e10a85e28b7102f2f8b012e510cd82b45fa
Kasper Bak <dan.echevarri@gmail.com>
2024-12-31 15:27:02 +0100

    Transform live links into client-links

--- src/app/ui/Card.tsx
+++ src/app/ui/Card.tsx
@@ -19,14 +19,9 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({ image, page, title, live, subtitle }) => {
   return (
     <div className="focus:shadow-outline focus:outline-none flex flex-col items-center justify-center rounded-3xl relative ">
-      <Link href={`projects/${page}`}>
-        <button>Go</button>
-      </Link>
-      <a
-        href={`${live}`}
+      <Link
+        href={`projects/${page}`}
         className="border transition-transform lg:hover:scale-90 overflow-hidden duration-500 rounded-3xl"
-        target="_blank"
-        rel="noopener noreferrer"
       >
         <Image
           className="md:p-10 bg-black transition-transform lg:hover:scale-125 hover:cursor-pointer  duration-500 object-center object-cover border rounded-3xl"
@@ -35,7 +30,7 @@ const Card: React.FC<CardProps> = ({ image, page, title, live, subtitle }) => {
           width={image.width}
           height={image.height}
         />
-      </a>
+      </Link>
       <p className="font-semibold text-xl mt-3 md:mt-8">{title}</p>
       <p>{subtitle}</p>
     </div>
