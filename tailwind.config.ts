bdc77cdf75b1152894dcd14b75ba9b8b00f7a257
Kasper Bak <dan.echevarri@gmail.com>
2025-01-03 16:09:02 +0100

    Fix types

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,8 @@ export default {
     },
     keyframes: {
       fadeInUp: {
-        "0%": { opacity: 0, transform: "translateY(20px)" },
-        "100%": { opacity: 1, transform: "translateY(0)" },
+        "0%": { opacity: "0", transform: "translateY(-20px)" },
+        "100%": { opacity: "1", transform: "translateY(0)" },
       },
     },
   },
