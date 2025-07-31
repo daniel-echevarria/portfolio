2bf957de34a614cf01f201e263877f45cd2b7b19
Kasper Bak <dan.echevarri@gmail.com>
2025-07-31 17:18:39 +0200

    Improve font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
-import { inter } from "./ui/fonts";
 import Nav from "@/app/ui/Nav";
 
 const geistSans = localFont({
@@ -13,6 +13,12 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-ibm-plex-sans",
+  weight: ["300", "400", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -28,9 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto`}
+        className={`${ibmPlexSans.variable} ${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto`}
       >
-        <div className="grid grid-rows-[50px_1fr_50px] lg:grid-rows-[50px_1fr_50px] items-center  min-h-screen p-8 sm:px-16k">
+        <div className="grid grid-rows-[50px_1fr_50px] lg:grid-rows-[50px_1fr_50px] items-center min-h-screen p-8 sm:px-16">
           <Nav />
           {children}
           <footer className="w-full my-14 sm:my-14 animate-fade-in-up opacity-0">
